92f1fefe737e2d45ed055d6109529d0d27d7315b
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2025-03-09 22:04:22 -0300

    Add apps genstub and genwrap, both based on template module.jinja

---
+++ hdltools/apps/genwrap.py
@@ -0,0 +1,46 @@
+#
+# Copyright (C) 2025 HDLtools Project
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+"""This script generates a wrapper for the specified module."""
+
+import argparse
+import logging
+import sys
+
+from hdltools.mod_parse import ModParse
+from hdltools.gen_file import GenFile
+
+
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--top')
+parser.add_argument('--suffix', default='_wrap')
+parser.add_argument('--output')
+parser.add_argument('file')
+args = parser.parse_args()
+
+modules = ModParse(args.file)
+names = modules.get_names()
+
+if not len(names) or (args.top and args.top not in names):
+    logging.error('module not found')
+    sys.exit(1)
+
+if not args.top:
+    args.top = names[0]
+
+module = modules.get_module(args.top)
+module['name'] = args.top
+module['suffix'] = args.suffix
+
+top = GenFile()
+top.render('module', module)
+
+if not args.output:
+    print(top)
+else:
+    top.write(args.output)
