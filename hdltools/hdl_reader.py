fb24d94179c721d090dbdbc35df3e294cf5ace8a
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2025-03-29 18:24:11 -0300

    Rewritten HdlSanitize as HDLReader

---
+++ hdltools/hdl_reader.py
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2025 HDLtools Project
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+"""
+Reads and sanitizes the input HDL code by removing comments, extra whitespaces
+and newlines.
+"""
+
+import re
+
+
+class HDLReader:
+    """Reads and sanitizes the input HDL code."""
+
+    def __init__(self, code=''):
+        self.code = code
+
+    def read_file(self, path):
+        """Reads the HDL code from file."""
+        with open(path, 'r', encoding='utf-8') as fobj:
+            self.code = fobj.read()
+
+    def set_code(self, code):
+        """Directly sets the HDL code."""
+        self.code = code
+
+    def get_code(self, is_vhdl=False):
+        """Retrieves the sanitized HDL code."""
+        if is_vhdl:
+            text = re.sub(r'--[^\n]*', '', self.code)
+        else:
+            text = re.sub(r'//[^\n]*', '', self.code)
+            text = re.sub(r'/\*.*?\*/', '', text, flags=re.DOTALL)
+        return re.sub(r'\s+', ' ', text).strip()
