cbe3152ee5921306fa88ad4386256e743b09dd76
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2025-04-05 23:57:03 -0300

    apps: simplified employing HDLController

--- hdltools/apps/genstub.py
+++ hdltools/apps/genstub.py
@@ -6,45 +6,14 @@
 
 """This script generates a stub for the specified module."""
 
-import logging
-import sys
-
 from hdltools.cli_parser import cli_parser
-from hdltools.hdl_reader import HDLReader
-from hdltools.mod_parser import ModParser
-from hdltools.hdl_writer import HDLWriter
+from hdltools.hdl_controller import HDLController
 
-logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
-
-args = cli_parser('stub')
-
-hdl_reader = HDLReader()
-try:
-    hdl_reader.read_file(args.file)
-except (OSError, UnicodeDecodeError) as e:
-    logging.error(f'{e}')
-    sys.exit(1)
-hdl_code = hdl_reader.get_code()
-
-mod_parser = ModParser(hdl_code)
-mod_parser.parse()
-
-module_names = mod_parser.get_names()
-if not len(module_names) or (args.top and args.top not in module_names):
-    logging.error('module not found')
-    sys.exit(1)
-
-if not args.top:
-    args.top = module_names[0]
-
-module_info = mod_parser.get_module(args.top)
-module_info['name'] = args.top
-module_info['suffix'] = args.suffix
-
-hdl_writer = HDLWriter()
-hdl_writer.render('stub', module_info)
+args = cli_parser('wrap')
+ctrl = HDLController('wrap')
+code = ctrl.generate(args.file, args.top, args.suffix)
 
 if not args.output:
-    print(hdl_writer.get_code())
+    print(code)
 else:
-    hdl_writer.write_file(args.output)
+    ctrl.write(args.output)
--- hdltools/apps/modcomp.py
+++ hdltools/apps/modcomp.py
@@ -7,88 +7,21 @@
 """This script compares two Verilog modules."""
 
 import difflib
-import logging
 import sys
 
 from hdltools.cli_parser import cli_parser
-from hdltools.hdl_reader import HDLReader
-from hdltools.mod_parser import ModParser
-from hdltools.hdl_writer import HDLWriter
+from hdltools.hdl_controller import HDLController
 
-logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
 args = cli_parser('comp')
+ctrl = HDLController('comp')
+mod1 = ctrl.generate(args.files[0], args.top1)
+mod2 = ctrl.generate(args.files[1], args.top2)
 
-#
-# Module 1
-#
-
-hdl_reader = HDLReader()
-try:
-    hdl_reader.read_file(args.files[0])
-except (OSError, UnicodeDecodeError) as e:
-    logging.error(f'{e}')
-    sys.exit(1)
-hdl_code = hdl_reader.get_code()
-
-mod_parser = ModParser(hdl_code)
-mod_parser.parse()
-
-module_names = mod_parser.get_names()
-if not len(module_names) or (args.top1 and args.top1 not in module_names):
-    logging.error('first module not found')
-    sys.exit(1)
-
-if not args.top1:
-    args.top1 = module_names[0]
-
-module_info = mod_parser.get_module(args.top1)
-module_info['name'] = args.top1
-
-hdl_writer = HDLWriter()
-hdl_writer.render('comp', module_info)
-
-module1 = hdl_writer.get_code()
-
-#
-# Module 2
-#
-
-hdl_reader = HDLReader()
-try:
-    hdl_reader.read_file(args.files[1])
-except (OSError, UnicodeDecodeError) as e:
-    logging.error(f'{e}')
-    sys.exit(1)
-hdl_code = hdl_reader.get_code()
-
-mod_parser = ModParser(hdl_code)
-mod_parser.parse()
-
-module_names = mod_parser.get_names()
-if not len(module_names) or (args.top2 and args.top2 not in module_names):
-    logging.error('second module not found')
-    sys.exit(1)
-
-if not args.top2:
-    args.top2 = module_names[0]
-
-module_info = mod_parser.get_module(args.top2)
-module_info['name'] = args.top2
-
-hdl_writer = HDLWriter()
-hdl_writer.render('comp', module_info)
-
-module2 = hdl_writer.get_code()
-
-#
-# Diff
-#
-
-if module1 == module2:
-    logging.info('modules are equal')
+if mod1 == mod2:
+    print('INFO: modules are equal')
 else:
-    logging.error('modules have differences')
-    diff = list(difflib.ndiff(module1.splitlines(), module2.splitlines()))
-    print("\n".join(diff))
+    print('ERROR: modules are different\n')
+    diff = list(difflib.ndiff(mod1.splitlines(), mod2.splitlines()))
+    print('\n'.join(diff))
     sys.exit(1)
